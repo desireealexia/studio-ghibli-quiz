b4b403547390fd32c5ecd2b77b886cb735369124
Desiree Williams-Forde <desireealexia19@gmail.com>
2024-09-09 11:53:24 +0100

    Update footer content and change file path for javascript files

---
+++ assets/javascript/questions.js
@@ -0,0 +1,113 @@
+// Storing questions and answers for the quiz
+// Code inspired by: How To Make Quiz Website Using HTML CSS And Javascript
+// Source: Codehal
+// URL: https://www.youtube.com/watch?v=Vp8x8-reqZA
+
+let questions = [
+    {
+        numb: 1,
+        question: "In which movie does a young girl named Satsuki and her sister Mei move to the countryside and discover magical creatures?",
+        answer: "My Neighbor Totoro",
+        options: [
+            "Spirited Away",
+            "My Neighbor Totoro",
+            "Ponyo",
+            "Howl's Moving Castle"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The film 'Kiki's Delivery Service' is set in a fictional European city and follows a young witch who starts a delivery business.",
+        answer: "True",
+        options: [
+            "True",
+            "False"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the primary setting of the film 'Castle in the Sky'?",
+        answer: "A floating island called Laputa",
+        options: [
+            "A floating island called Laputa",
+            "A bustling metropolis",
+            "A magical forest",
+            "An underwater kingdom"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which movie features this robot statue?",
+        image: "assets/images/question-images/quiz-img-q4-robot.webp",
+        answer: "Castle in the Sky",
+        options: [
+            "Castle in the Sky",
+            "The Secret World of Arrietty",
+            "The Cat Returns",
+            "Nausicaä of the Valley of the Wind"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In 'Princess Mononoke,' who is the leader of the boar tribe?",
+        answer: "Okkoto",
+        options: [
+            "San",
+            "Jigo",
+            "Ashitaka",
+            "Okkoto"
+        ]
+    },
+    {
+        numb: 6,
+        question: "The protagonist of 'The Wind Rises,' Jiro Horikoshi, is based on a real historical figure.",
+        answer: "True",
+        options: [
+            "True",
+            "False"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is the main conflict in the film 'Nausicaä of the Valley of the Wind'?",
+        answer: "A battle between humans and giant insects",
+        options: [
+            "A battle between humans and giant insects",
+            "A war over magical artefacts",
+            "The struggle to save a dying princess",
+            "A quest to find a lost kingdom"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What is the name of this character?",
+        image: "assets/images/question-images/quiz-img-q8-baron.webp",
+        answer: "Baron",
+        options: [
+            "Baron",
+            "Muta",
+            "Jiji",
+            "Luna"
+        ]
+    },
+    {
+        numb: 9,
+        question: "In 'Whisper of the Heart,' what is the name of the antique shop that plays a significant role in the film?",
+        answer: "The Baron's Shop",
+        options: [
+            "The Cat's Store", 
+            "The Antique Shop of Dreams",
+            "The Baron's Shop", 
+            "The Moonlight Shop"
+        ]
+    },
+    {
+        numb: 10,
+        question: "'The Tale of the Princess Kaguya' was created using CGI animation.",
+        answer: "False",
+        options: [
+            "True",
+            "False"
+        ]
+    }
+];
---
+++ assets/javascript/script.js
@@ -0,0 +1,43 @@
+// Modal functionality script
+// Code adapted from: W3Schools (https://www.w3schools.com/howto/howto_css_modals.asp)
+
+// Get the modals
+var aboutModal = document.getElementById("about-modal");
+var instructionsModal = document.getElementById("instructions-modal");
+var contactModal = document.getElementById("contact-modal");
+
+// Get the navigation links
+var aboutLink = document.getElementById("about-link");
+var instructionsLink = document.getElementById("instructions-link");
+var contactLink = document.getElementById("contact-link");
+
+// Get the close buttons
+var closeButtons = document.querySelectorAll(".close-button");
+
+// When the user clicks on a link, open the corresponding modal
+aboutLink.onclick = function() {
+    aboutModal.style.display = "block";
+}
+
+instructionsLink.onclick = function() {
+    instructionsModal.style.display = "block";
+}
+
+contactLink.onclick = function() {
+    contactModal.style.display = "block";
+}
+
+// When the user clicks on a close button, close the modal
+closeButtons.forEach(function(button) {
+    button.onclick = function() {
+        button.parentElement.parentElement.style.display = "none";
+    }
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+}
+// End of modal functionality script --------------------------------------------------------
